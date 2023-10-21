91c3b86d4c6cee9a2ad56bfaae087d12d03332fc
SUHAIB SABIR <suhaibsabir110@gmail.com>
2023-10-21 09:53:41 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+
+var myvariable=prompt("entre the value")
+document.write("VALUE AFTER VARIABLE DECLARATION IS"+ " "+myvariable+"<br>")
+
+myvariable++;
+document.write("VALUE AFTER INCRIMENT IS\n"+" "+myvariable+"<br>")
+
+
+var num1=+prompt("entre the value")
+var num2=+prompt("entre the value")
+document.write(num1+num2)
+
+
+var a
+var b
+
+a=3000;
+b=5;
+
+document.write("the price of tickets is"+a*b + "<br>")
+
+
+
+var ctemp=3;
+var ftemp=(ctemp *9/5)+32;
+
+document.write("the temperature after changes is"+(ctemp=ftemp)+"<br>")
+
+
+var ftemp=3;
+var ctemp=(ftemp -32)*9/5;
+
+document.write("the temperature after changes is"+(ftemp=ctemp)+"<br>")
+
+
+
+
+var priceOfItem1=300;
+document.write("the amount of first item is"+ +priceOfItem1+ "<br>")
+var quantitys=350;
+document.write("the amount of second item"+quantitys+ "<br>")
+var item2=2;
+document.write("quantity of scond item is"+item2+"<br>")
+
+var item=200;
+document.write("amount of second item"+item+" <br>")
+document.write("total bill"+(priceOfItem1*quantitys+(item2*item))+"<br>")
+
+
+
+
+var ali=550;
+var total=800;
+
+
+document.write("Ali got the percentage in FSC"+" "+total/ali)*100 +"<br>";
+
+
+var ahmed=750;
+var total=800;
+document.write("Ahmed got the percentage in FSC"+" "+total/ahmed)*100;
+
+
